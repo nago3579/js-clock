40ab1318aa6d8bcd0fc7e2fc0b043e4c8296b496
nadiv gold <nago3579@colorado.edu>
2020-04-23 12:12:42 -0600

    Updates

---
+++ precache-manifest.28f6cf278152bcb441e97c28f5c304ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f57cb74f3390b2941a07b03cdbe05883",
+    "url": "/js-clock/index.html"
+  },
+  {
+    "revision": "2a36ec2aa481b2d7718d",
+    "url": "/js-clock/static/css/main.d8665f84.chunk.css"
+  },
+  {
+    "revision": "dc7d177b896d41bf875f",
+    "url": "/js-clock/static/js/2.9422306a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/js-clock/static/js/2.9422306a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a36ec2aa481b2d7718d",
+    "url": "/js-clock/static/js/main.05926883.chunk.js"
+  },
+  {
+    "revision": "2f81796f9cdd3dcd000f",
+    "url": "/js-clock/static/js/runtime-main.39c56044.js"
+  }
+]);
